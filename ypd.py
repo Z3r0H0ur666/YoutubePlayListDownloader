9cce5069908b6f4af9967f1b94eaefae58dcb692
NullC0d3 <ahmed.m.ameen@gmail.com>
2022-05-27 18:21:51 +0200

    Update ypd.py

--- ypd.py
+++ ypd.py
@@ -1,6 +1,18 @@
 from pytube import Playlist
-p = Playlist('https://www.youtube.com/playlist?list=PLpXus3JSQYPAGM2yGak01xZD7tuKXB3gQ')
+p = input("Enter the url of the playlist")
 
+print("""\033[91m
+   _   _       _ _  _____ ___      _ ____  
+ | \ | |     | | |/ ____/ _ \    | |___ \ 
+ |  \| |_   _| | | |   | | | | __| | __) |
+ | . ` | | | | | | |   | | | |/ _` ||__ < 
+ | |\  | |_| | | | |___| |_| | (_| |___) |
+ |_| \_|\__,_|_|_|\_____\___/ \__,_|____/ 
+                                          
+                                          \033[00m
+				\033[93mv.1.2
+				By NullC0d3\033[00m
+""")
 print(f'Downloading: {p.title}')
 
 for video in p.videos:
